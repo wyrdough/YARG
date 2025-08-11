fb2536fbfe3fd14d6cf5023769a4a488f5456332
wyrdough <nathan@nwacg.net>
2025-08-11 14:46:21 -0400

    profile onboarding wip

---
+++ Assets/Script/Menu/Common/Dialogs/Onboarding/FiveFretBindingDialog.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace YARG.Menu.Dialogs
+{
+    public class FiveFretBindingDialog : InputBindingDialog
+    {
+        [SerializeField]
+        private FiveFretBindingImages _images;
+
+        protected override void OnEnable()
+        {
+            _images.GreenFret.gameObject.SetActive(false);
+            _images.RedFret.gameObject.SetActive(false);
+            _images.YellowFret.gameObject.SetActive(false);
+            _images.BlueFret.gameObject.SetActive(false);
+            _images.OrangeFret.gameObject.SetActive(false);
+            _images.StrumUp.gameObject.SetActive(false);
+            _images.StrumDown.gameObject.SetActive(false);
+            _images.Whammy.gameObject.SetActive(false);
+
+            base.OnEnable();
+        }
+
+
+        [System.Serializable]
+        public struct FiveFretBindingImages
+        {
+            public Image FiveFretBase;
+            public Image GreenFret;
+            public Image RedFret;
+            public Image YellowFret;
+            public Image BlueFret;
+            public Image OrangeFret;
+            public Image StrumUp;
+            public Image StrumDown;
+            public Image Whammy;
+        }
+    }
+}
---
+++ Assets/Script/Menu/Common/Dialogs/Onboarding/InputBindingDialog.cs
@@ -0,0 +1,19 @@
+﻿namespace YARG.Menu.Dialogs
+{
+    public class InputBindingDialog : ImageDialog
+    {
+
+        protected override void OnEnable()
+        {
+            // TODO: Figure out how to get the button presses even when they aren't yet bound
+            base.OnEnable();
+        }
+
+        protected override void OnBeforeClose()
+        {
+            // TODO: Undo whatever you did to get the button presses
+            base.OnBeforeClose();
+        }
+
+    }
+}
